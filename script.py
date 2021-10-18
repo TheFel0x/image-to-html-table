2c81f2e8a7b72fa027378f2239b9b507eb02f5c7
TheFel0x <43345523+TheFel0x@users.noreply.github.com>
2021-10-18 14:23:57 +0200

    interactive basics

--- script.py
+++ script.py
@@ -1,38 +1,86 @@
-#test
 import os, argparse, sys
 from PIL import Image
 
 def main():
- 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input",type=str,help="image file")
-    parser.add_argument("-o","--output",type=str,help="specify output directory")
-    parser.add_argument("-n","--name",type=str,help="name of output file (default name is 'output.html')")
-    parser.add_argument("-t","--type",type=str,choices=["code","website"],help="'website' puts out the file with head and body tags while 'code' only saves the table part in a file")
-    parser.add_argument("-b","--border",action='store_true',help="turns on border")
-    parser.add_argument("--nocollapse",action='store_true',help="disables border collapse")
-    parser.add_argument("-v","--overwrite",action='store_true',help="can replace existing output file")
-    parser.add_argument("-r","--resize",type=int,help="resizes image. value is new width in pixel. --> changes amounnt of pixels")
-    parser.add_argument("-s","--size",type=int,help="width of the resulting table in pixel --> changes size of table")
 
-    args = parser.parse_args()
+    # TODO: remove after interactive input finished
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("input",type=str,help="image file")
+    # parser.add_argument("-o","--output",type=str,help="specify output directory")
+    # parser.add_argument("-n","--name",type=str,help="name of output file (default name is 'output.html')")
+    # parser.add_argument("-t","--type",type=str,choices=["code","website"],help="'website' puts out the file with head and body tags while 'code' only saves the table part in a file")
+    # parser.add_argument("-b","--border",action='store_true',help="turns on border")
+    # parser.add_argument("--nocollapse",action='store_true',help="disables border collapse")
+    # parser.add_argument("-v","--overwrite",action='store_true',help="can replace existing output file")
+    # parser.add_argument("-r","--resize",type=int,help="resizes image. value is new width in pixel. --> changes amounnt of pixels")
+    # parser.add_argument("-s","--size",type=int,help="width of the resulting table in pixel --> changes size of table")
 
-    in_file = args.input
+    # args = parser.parse_args()
+
+    print("Enter path of the input image.")
+    in_file = input("path: ")
     
-    out_path = args.output if not args.output == None else os.getcwd()
-    do_overwrite = args.overwrite
-    out_name = args.name if not args.name == None else "output.html"
-    is_full_html = True if args.type == "code" else False
-    out_file = os.path.join(out_path,out_name) 
+    # in_file = args.input
+    print("Where do you want to output the file?\nEnter full directory or leave empty to save in working directory.")
+    inp = input("path: ")
+    if inp == None:
+        out_path = os.getcwd()
+    else:
+        out_path = inp
+    # out_path = args.output if not args.output == None else os.getcwd()
+    
+    print("Overwrite the existing file if necessary? (y/N)")
+    inp = input("y/N: ")
+    if inp.str.lower() == "y" or inp.str.lower() == "yes":
+        do_overwrite = True
+    else:
+        do_overwrite = False
+    #do_overwrite = args.overwrite
+    
+    print("What name should the output have? (leave blank for \"output.html\")")
+    inp = input("filename: ")
+    if inp == None:
+        out_name =  "output.html"
+    else:
+        out_name = inp
+    #out_name = args.name if not args.name == None else "output.html"
 
-    style_border = "border: 1px solid black;" if args.border else ""
-    style_collapse = "border-collapse: none;" if args.nocollapse else "border-collapse: collapse;"
+    print("Do you want the other HTML tags to be automatically added? (Working HTML page) (Y/n)")
+    inp = input("Y/n: ")
+    if inp.str.lower() == "n" or inp.str.lower() == "no":
+        is_full_html = False
+    else:
+        is_full_html = True
+    #is_full_html = True if args.type == "code" else False
+
+    print("Note: your file will be saved as:")
+    out_file = os.path.join(out_path,out_name)
+    print(out_file) 
+
+    print("Want a border around your table cells (pixels)? (y/N)")
+    inp = input("y/N: ")
+    if inp.str.lower() == "y" or inp.str.lower() == "yes":
+        border = True
+    else:
+        border = False
+
+    style_border = "border: 1px solid black;" if border else ""
+
+    print("Prevent borders (if enabled) from collapsing? (y/N)")
+    inp = input("y/N: ")
+    if inp.str.lower() == "y" or inp.str.lower() == "yes":
+        nocollapse = True
+    else:
+        nocollapse = False
+
+    style_collapse = "border-collapse: none;" if nocollapse else "border-collapse: collapse;"
 
     im = Image.open(in_file)
 
-    if not args.resize == None:
-        size = (args.resize,args.resize*im.height//im.width)
-        im = im.resize(size)
+    # TODO: make resizing possible again
+    # if not args.resize == None:
+    #     size = (args.resize,args.resize*im.height//im.width)
+    #     im = im.resize(size)
 
     if not do_overwrite and os.path.isfile(out_file):
         print("File exists. Terminating script. Add '--overwrite' / '-v' or change the outputs name with '--name' / '-n'")
@@ -45,8 +93,9 @@ def main():
 
     style = style_border+style_collapse
 
-    table_width = im.size[0] if args.size == None else args.size
-    table_height = im.size[1] if args.size == None else im.size[1]*args.size//im.size[0]
+    # TODO: make size settings possible again
+    table_width = im.size[0] # if args.size == None else args.size
+    table_height = im.size[1] # if args.size == None else im.size[1]*args.size//im.size[0]
 
     f.write('<table style="'+style+'width:'+str(table_width)+'px;height:'+str(table_height)+'px;">')
 
